d9e7e73dfaf9d15128da26dfd84aa0b6543d6f34
ddrmaniak <ddrmaniak_NG@hotmail.com>
2012-11-26 13:24:29 -0800

    Azure client added

---
+++ foodApp/Models/azureHelper.cs
@@ -0,0 +1,33 @@
+﻿using System.Configuration;
+using Microsoft.WindowsAzure.ServiceRuntime;
+
+namespace AzureWebApplication
+{
+    public class ConfigHelper
+    {
+        /// <summary>
+        /// Returns the value of the configuration setting called "OldSettingName1" 
+        /// from either web.config, or the Azure Role Environment.
+        /// </summary>
+        public static string OldSettingName1
+        {
+            get
+            {
+                return RoleEnvironment.IsAvailable ?
+                       RoleEnvironment.GetConfigurationSettingValue("OldSettingName1") :
+                      ConfigurationManager.AppSettings["OldSettingName1"];
+            }
+        }
+
+        /// <summary>
+        /// Returns the value of the configuration setting called ”settingName”
+        /// from either web.config, or the Azure Role Environment.
+        /// </summary>
+        public static string GetSettingAsString(string settingName)
+        {
+            return RoleEnvironment.IsAvailable ?
+                   RoleEnvironment.GetConfigurationSettingValue(settingName) :
+                  ConfigurationManager.AppSettings[settingName];
+        }
+    }
+}
